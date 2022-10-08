d10ec2e1a196c59cc544a617d95a8e6b41d42f80
Ben <65823405+SapezB@users.noreply.github.com>
2022-10-08 12:12:55 +0100

    Update main.java

--- Adventure Game/miniproject/main.java
+++ Adventure Game/miniproject/main.java
@@ -11,7 +11,7 @@ public class main {
 
     }
     
-    public void CreateWindow(JFrame window){
+    public void CreasdsteWindow(JFrame window){
 
         window.setTitle("Adventure Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
